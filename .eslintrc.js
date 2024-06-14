83f5a6d66aa1f39513cdf273aa9c599ae0fdc2f1
Daniel Oakman <141111365+danoaky-tiny@users.noreply.github.com>
2024-06-14 11:31:43 +1000

    INT-3313: Refactored .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,44 @@
 module.exports = {
-
   env: {
     browser: true,
-    node: true
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
   },
   overrides: [
     {
       files: ['*.svelte'],
       parser: 'svelte-eslint-parser',
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
       parserOptions: {
         project: './tsconfig.json',
-        sourceType: 'module',
         parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.svelte']
       },
       rules: {
-        "eqeqeq": "error"
-      }
+        eqeqeq: 'error',
+      },
     },
     {
       files: ['*.ts'],
       extends: 'plugin:@tinymce/standard',
       parserOptions: {
-        project: "tsconfig.json",
-        sourceType: "module"
+        project: './tsconfig.json',
       },
       rules: {
-        "@tinymce/prefer-fun": "off",
-        "@typescript-eslint/no-duplicate-imports": "off",
-        "@typescript-eslint/no-parameter-properties": "off"
-      }
-    }
-  ]
-}
+        '@tinymce/prefer-fun': 'off',
+        '@typescript-eslint/no-duplicate-imports': 'off',
+        '@typescript-eslint/no-parameter-properties': 'off',
+      },
+    },
+  ],
+};
